1c1129e94581be33d724fca7ccefe216a0b27f4a
Coder01abhi <abhishekpanday01@rediffmail.com>
2022-05-06 10:29:42 +0530

    Initial Commit

---
+++ compare.py
@@ -0,0 +1,9 @@
+def comp(a,b):
+    if a == b:
+        print(a," is equal to ",b)
+    elif a>b :
+        print(a," is greater than ",b)
+    else :
+        print(a," is smaller than ",b)      
+
+comp(6,8)
